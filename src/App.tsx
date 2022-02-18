fe56587a7b1aef902b73130c1c2254383270ecbf
César Júnior <cesar.jnr@hotmail.com>
2022-02-17 22:40:45 -0300

    Create a global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import { Login } from "./pages/Login";
 import { SignUpModal } from './pages/Login/SignUpModal';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  
+  body {
+    height: 100%;
+    margin: 0;
+    color: rgb(255, 255, 255);
+  }
+`;
+
 export const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />}>
-        <Route path="i/flow/signup" element={<SignUpModal />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <React.Fragment>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route path="i/flow/signup" element={<SignUpModal />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.Fragment>
 );
